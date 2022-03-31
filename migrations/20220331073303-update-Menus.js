d725bd8163c3e29bcac60de5ae98103019dab897
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-03-31 14:55:36 +0530

    feat: add migrations and models for tables
    add foreign key using update migration

---
+++ migrations/20220331073303-update-Menus.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     *
+     *
+     */
+
+    await queryInterface.addColumn("Menus", "restaurant_id", {
+      type: Sequelize.INTEGER,
+      onDelete: "cascade",
+      references: {
+        model: "Resturants",
+        key: "id",
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeColumn("Menus", "restaurant_id");
+  },
+};
---
+++ models/menus.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Menus extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Resturants }) {
+      // define association here
+
+      Menus.belongsTo(Resturants, {
+        foreignKey: "resturant_id",
+        onDelete: "cascade",
+      });
+    }
+  }
+  Menus.init(
+    {
+      name: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      rating: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Menus",
+    }
+  );
+  return Menus;
+};
